c02e07a5c3841871d96c3a1703d9c6495792a94e
Erfan Najimi <erfannajimi123@gmail.com>
2024-08-18 14:53:00 +0000

    Add favourite star icon to dashboard

--- static/js/script.js
+++ static/js/script.js
@@ -1,16 +1,18 @@
 // Dashboard
 
-const project_info = document.getElementById('project-info');
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+const project_info = document.getElementsByClassName('project-info');
 const expand_toggle = document.getElementById('expand-toggle');
 
 expand_toggle.addEventListener('click', () => {
-    if (project_info.className.includes('active')){
-        project_info.style.display = 'none';
-        project_info.className -= 'active';
 
-    } else {
-        project_info.style.display = 'block';
-        project_info.className += 'active';
+    for (let i = 0; i < project_info.length; i++) {
+        if (project_info[i].style.display == 'none') {
+            project_info[i].style.display = 'block';
+        } else {
+            project_info[i].style.display = 'none';
+        }
     }
-
 })
