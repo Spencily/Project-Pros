b92a791dbeacc6d948674eea11f38124a515f97f
Erfan Najimi <erfannajimi123@gmail.com>
2024-08-18 21:02:40 +0000

    add emoji form functionality in dashboard version 1

--- project/views.py
+++ project/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from .models import project_detail
+from .forms import EmojiSelection
 
 # Create your views here.
 class ProjectView(View):
@@ -14,11 +15,21 @@ def dashboard_content(request):
     search = request.GET.get("project-search")
     search_results = project_detail.objects.filter(title__icontains=search).values() if search != None else ''
 
+    if request == 'POST':
+        emoji_form = EmojiSelection(data=request.POST)
+        if emoji_form.is_valid():
+            emoji = emoji_form.save(commit=False)
+            emoji.account = request.user
+            emoji.save()
+    
+    emoji_form = EmojiSelection()
+
     return render(
         request,
         'project/dashboard.html',
         {
             'projects' : projects,
             'search_results' : search_results,
+            'emoji_form' : emoji_form,
         },
     )
