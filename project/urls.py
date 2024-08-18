19574b12ac0936c88caa67660810c71eab4c38f8
Erfan Najimi <erfannajimi123@gmail.com>
2024-08-18 09:44:25 +0000

    Add dashboard url path and link to view to render basic template

--- project/urls.py
+++ project/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import include, path
-
-from .views import ProjectView
+from .views import ProjectView, dashboard_content
 
 urlpatterns = [
+    path('dashboard/', dashboard_content, name='dashboard'),
     path('', ProjectView.as_view(), name='project'),
 ]
