0758b055ff4b808d2cbcf2d79048143df6cdb339
Erfan Najimi <erfannajimi123@gmail.com>
2024-08-19 08:06:16 +0000

    Add svg image to static

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -1,8 +1,18 @@
 // Dashboard
 
-function toggle() {
-    document.getElementById('project-info').style().display = 'block'
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
-}
+const project_info = document.getElementsByClassName('project-info');
+const expand_toggle = document.getElementById('expand-toggle');
 
-document.getElementById('expand-toggle').addEventListener(onclick, toggle);
+expand_toggle.addEventListener('click', () => {
+
+    for (let i = 0; i < project_info.length; i++) {
+        if (project_info[i].style.display == 'none') {
+            project_info[i].style.display = 'block';
+        } else {
+            project_info[i].style.display = 'none';
+        }
+    }
+})
