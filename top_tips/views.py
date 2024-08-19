1ac886acb721f237c08280d8e8d2d8573fdd1f86
Juan Pinson Moreno <juan@fourrooms.co.uk>
2024-08-19 10:15:50 +0000

    add toptips resgistration, create tips.html template and query the database for all tips

--- top_tips/views.py
+++ top_tips/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.views import View
+from .models import TopTips
 
-# Class-Based View (CBV)
+
 class TopTipsView(View):
     def get(self, request):
-        return render(request, 'top_tips/top_tips.html')
+        tips = TopTips.objects.all()  # Query the database for all tips
+        # Pass the tips to the template
+        return render(request, 'top_tips/tips.html', {'tips': tips})
